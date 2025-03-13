ac8a0d59846c66969d2be6030c44c718e0ed4bc4
Ketan Patel <ketanpatel19102000@gmail.com>
2025-03-13 12:23:46 +0530

    Updated version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.unicorndevelopers.inkspiration"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         buildConfigField("String", "API_KEY", "\"${localProperties["QUOTE_API_KEY"]}\"")
 
